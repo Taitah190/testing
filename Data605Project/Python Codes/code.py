feda4635d377c074b1160b582e0482560d84be8c
Ke <Taitah190@github.com>
2024-04-17 15:36:49 -0400

    uploading the whole video to rethinkdb, might break the whole thing

--- Data605Project/Python Codes/code.py
+++ Data605Project/Python Codes/code.py
@@ -9,18 +9,51 @@ import pandas as pd
 rethink = r.RethinkDB()
 rethink.connect('localhost', 28015).repl()
 frames = []
-'''cap = cv2.VideoCapture('/home/ke/Desktop/database/testing/Data605Project/Python Codes/Rickroll.mp4')
+
+def split(array):
+    array = np.array(array)
+    red = array[:, :, 0:1]
+    green = array[:, :, 1:2]
+    blue = array[:, :, 2:3]
+    nb = []
+    nr = []
+    ng = []
+    for i in red:
+        tp = [item for sublist in i for item in sublist]
+        tp = [int(x) for x in tp]
+        nr.append(tp)
+        i = None
+    red = None
+    for j in green:
+        tp = [item for sublist in j for item in sublist]
+        tp = [int(x) for x in tp]
+        ng.append(tp)
+        j = None
+    green = None
+    for k in blue:
+        tp = [item for sublist in k for item in sublist]
+        tp = [int(x) for x in tp]
+        nb.append(tp)
+        k = None
+    blue = None
+    red = None
+    green = None
+    return nr, ng, nb
+
+cap = cv2.VideoCapture('/home/ke/Desktop/database/testing/Data605Project/Python Codes/Rickroll.mp4')
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-print( length )
-fps = cap.get(cv2.CAP_PROP_FPS)
-print(fps)
-for i in range(100):
+for i in range(length):
+    new = []
     cap.set(cv2.CAP_PROP_POS_FRAMES, i)
     ret, frame = cap.read()
-    frame = frame.tolist()
-    #y=np.array(frame)
-    frames.append(frame)
-    print(i/length)'''
+    frame = np.array(frame)
+    red, green, blue = split(frame)
+    name = str("frame_"+i)
+    rethink.db('Project').table_create(name).run()
+    for i in range(len(red)):
+        new.append({"red":red[i], "green":green[i], "blue":blue[i]})
+    rethink.db('Project').table(name).insert(new).run()
+    print(i/length)
 #rethink.db("test").table_create("array"run()
 #print(frame.ndim)
 
@@ -36,26 +69,38 @@ with open("/home/ke/Desktop/database/testing/Data605Project/Python Codes/test2.j
 #print(frames[12].ndim)
 #newarray = np.stack(frames)
 #print(newarray.shape)
-new = []
+
 ###Line of Submission###
-im = cv2.imread("/Users/xukexukexuke/test/testing/Data605Project/Python Codes/earth.jpg")
-#print(im[1][1])
-#im = im.astype(str)
+
+'''new = []
+im = cv2.imread("/home/ke/Desktop/database/testing/Data605Project/Python Codes/earth.jpg")
 im = np.array(im)
-data = {'array_3d': im}
-red = im[:, :, 0:1]
+red, green, blue = split(im)
+for i in range(len(red)):
+    new.append({"red":red[i], "green":green[i], "blue":blue[i]})
+    #print(i)
+rethink.db('Project').table('picture').insert(new).run()
+'''
+
+
+'''
 red = red.tolist()
 green = im[:, :, 1:2]
 green = green.tolist()
 blue = im[:, :, 2:3]
 blue = blue.tolist()
-#print(red[1:10])
+nb = []
+for k in blue:
+    nb.append([item for sublist in k for item in sublist])'''
+#rethink.db('Project').table_create('picture').run()
 '''for i in range(len(red)):
     frames.append({"red":red[i], "green":green[i], "blue":blue[i]})
-    print(i)
-print(type(frames[1]))'''
+    #print(i)
+print(len(red[2]))
 #rethink.db('Project').table_create('NewArray').run()
 #rethink.db('Project').table('Array').insert(frames).run()
 array = rethink.db('Project').table('Array').pluck('red').run()
-array = list(array)
-print(len(array))
+for document in array:
+    new.append(document)
+x = list(new[4].values())
+print(x[0][4][0])'''
